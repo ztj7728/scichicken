fa9100ba7a9d6cf54da0270d6c62865e1a76c40b
ztj7728 <30467430+ztj7728@users.noreply.github.com>
2018-05-01 00:27:12 +0800

    Update script-download-map.js
    Fixed download link.
    Fixed map erangel version to v11.
    Fixed map miramar version to v5。

--- script-download-map.js
+++ script-download-map.js
@@ -4,10 +4,10 @@ const axios = require('axios')
 const fs = require('fs')
 const mkdirp = require('mkdirp')
 
-// example: https://tiles1.pubgmap.net/maptiles/miramar/v3/5/31/31.png
+// example: https://tiles2-v2.pubgmap.net/maptiles/miramar/v3/5/31/31.png
 const zooms = [0, 1, 2, 3, 4] // 5 might be too many
-const baseurl = 'https://tiles3.pubgmap.net/maptiles' // tiles0-3 can be used, I think they are the same
-const maps = ['elevation-erangel/v5', 'erangel/v10', 'miramar/v3', 'elevation-miramar/v2']
+const baseurl = 'https://tiles2-v2.pubgmap.net/tiles' // tiles0-3 can be used, I think they are the same
+const maps = ['elevation-erangel/v5', 'erangel/v11', 'miramar/v5', 'elevation-miramar/v2']
 const downloadLoc = './static/maptiles'
 
 async function downloadImage (map, z, x, y) {
